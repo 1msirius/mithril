b98db23b875bc02e276a49f16c43c973d36a7323
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-16 15:19:49 +0530

    Update

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,8 +6,8 @@ export function Navbar() {
     <section className="flex justify-between items-center">
       <div className="flex items-center">
         <a href="/" className="flex items-center">
-          <Image src="/logo.png" alt="Mithril AI Logo" width={35} height={35} />
-          <h1 className="text-3xl font-semibold ml-3">Mithril</h1>
+          <Image src="/logo.png" alt="Mithril AI Logo" width={30} height={30} />
+          <h1 className="text-2xl font-semibold ml-3">MITHRIL</h1>
         </a>
       </div>
       <div className="flex space-x-4 text-xs font-normal">
