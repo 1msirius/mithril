9de9441872b5a584852ea98c4f4394674e1e4e49
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-18 03:33:59 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center text-center h-[82vh]">
-      <h1 className="text-5xl mb-8">Open Source Gen AI Research Lab</h1>
-      <p className="text-[18px] leading-[28px] w-[616px] text-[#bbb8b8]">
+      <h1 className="text-[45px] mb-6">Open Source Gen AI Research Lab</h1>
+      <p className="text-[18px] leading-[28px] w-[600px] text-[#BABABA] font-normal">
         We are an open-source AI research and development company committed to
         advancing the training, fine-tuning, and inference of pre-trained large
         language models, while openly sharing the datasets.
