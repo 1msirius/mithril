7f7e5cd2f9440b30767237921431321ee174125b
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-19 22:49:27 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center text-center h-[82vh]">
-      <h1 className="text-[45px] mb-4">Open Science AI Research Lab</h1>
-      <p className="text-base w-[600px]  font-normal">
-        We are an open-source AI research company focused on advancing
-        pre-trained language models. We aim to become the Linux of AI, with
-        everything—from our code to data pipelines—fully open and accessible.
+      <h1 className="text-5xl mb-7">Open Science AI Research Lab</h1>
+      <p className="text-base w-[600px] font-normal leading-[26px]">
+        We are an open-source AI research company focused on enhancing
+        pre-trained generative AI models. Our goal is to become the Linux of AI,
+        with everything <br />
+        from model weights to data pipelines fully open and accessible to all.
       </p>
     </div>
   );
