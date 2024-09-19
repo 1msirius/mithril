9a2c17003576494a60abaa2a7e82f5ec070cc797
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-20 03:50:30 +0530

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center text-center px-4 sm:px-72 h-[82vh]">
-      <h1 className="text-4xl pb-6">Understand the Universe</h1>
-      <p>
-        We are an open-source AI research and development company committed to
-        advancing the training, fine-tuning, and inference of pre-trained large
-        language models, while openly sharing the datasets used in the process.
-        Our vision is to become the &quot;Linux of AI,&quot; where
-        everything—from the codebase to the data pipeline—is open and accessible
-        to all.
+    <div className="flex flex-col items-center justify-center h-[82vh] text-center px-4">
+      <h1 className="text-4xl md:text-5xl mb-8">Our Mission</h1>
+      <p className="text-base mb-8 w-full max-w-[600px] leading-[26px]">
+        We are an open-source AI research company dedicated to advancing
+        pre-trained generative AI models. Our mission is to become the Linux of
+        AI, making everything from model architectures and weights to training
+        data <br /> and development pipelines fully open, transparent, and
+        accessible to all.
       </p>
+      <div className="flex gap-4">
+        <a href="https://github.com/mithrilai">
+          <button className="bg-white text-black px-6 py-2 rounded-full text-base font-medium hover:bg-gray-200 transition-colors">
+            GitHub
+          </button>
+        </a>
+        <a href="https://discord.gg/jgy376Jyka">
+          <button className="bg-black text-white border border-[#787878] px-6 py-2 rounded-full text-base font-medium hover:bg-[#111111] transition-colors">
+            Join Us
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-[82vh] text-center px-4">
-      <h1 className="text-5xl mb-8">Open Science AI Research Lab</h1>
-      <p className="text-base mb-8 w-[600px] leading-[26px]">
-        We are an open-source AI research company focused on enhancing
+      <h1 className="text-4xl md:text-5xl mb-8">
+        Open Science AI Research Lab
+      </h1>
+      <p className="text-base mb-8 w-full max-w-[600px] leading-[26px]">
+        We are an open-source AI research company dedicated to advancing
         pre-trained generative AI models. Our goal is to become the Linux of AI,
         with everything <br />
         from model weights to data pipelines fully open and accessible to all.
