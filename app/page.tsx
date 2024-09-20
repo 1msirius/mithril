ebe9612fbbcd49be1b68b27445b57cea130fc148
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-20 23:47:12 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
       <h1 className="text-4xl md:text-5xl mb-8">
         Open Science AI Research Lab
       </h1>
-      <p className="text-[22px] mb-8 w-full max-w-sm">
-        Open models across diverse domains, for everyone, everywhere.
+      <p className="text-lg mb-8 w-full font-[family-name:var(--font-geist-mono)]">
+        Open models across diverse domains,
+        <br /> for everyone, everywhere.
       </p>
       <div className="flex gap-4">
         <a href="https://github.com/mithrilai">
@@ -14,7 +15,7 @@ export default function Home() {
           </button>
         </a>
         <a href="https://discord.gg/jgy376Jyka">
-          <button className="bg-black text-white border border-[#787878] px-6 py-2 rounded-full text-base font-medium hover:bg-[#111111] transition-colors">
+          <button className="bg-black text-[#EDEDED] border border-solid border-[#2B2B2B] px-6 py-2 rounded-full text-base font-medium hover:bg-[#111111] transition-colors">
             Join Us
           </button>
         </a>
