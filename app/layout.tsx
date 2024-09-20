12618c48f2a0ea61e054bb2efbc5408fd8c5d940
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-21 01:27:01 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,13 +49,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className="px-8 py-4 md:px-16 md:py-10 relative">
-        <Navbar />
-        <div className="absolute inset-0 -z-10">
-          <Particles3D />
+      <body className="relative">
+        <div className="px-8 py-4 md:px-16 md:py-10">
+          <Navbar />
+          <div className="absolute inset-0 -z-10">
+            <Particles3D />
+          </div>
+          {children}
+          <Footer />
         </div>
-        {children}
-        <Footer />
       </body>
     </html>
   );
