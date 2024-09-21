5f821e5c048c0642fe6422d8c36abc1405d105cf
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-21 16:13:53 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,15 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: true,  // Necessary for static exports in GitHub Pages
   },
-  trailingSlash: true,
-}
+  trailingSlash: true,  // Ensure directories end with a trailing slash
+  async exportPathMap(defaultPathMap) {
+    return {
+      ...defaultPathMap,
+      '/404': { page: '/not-found' },  // Define the custom 404 page
+    };
+  },
+};
 
 export default nextConfig;
