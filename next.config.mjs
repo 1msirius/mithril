f39a7162ad385e80c125592c579a48b3036a3eff
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-21 17:29:57 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,  // GitHub Pages prefers trailing slashes for directories
-};
-
-export default nextConfig;
+  images: { unoptimized: true },
+  // This ensures that assets are correctly referenced
+  assetPrefix: '.',
+}
